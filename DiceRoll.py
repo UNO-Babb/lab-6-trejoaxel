2e3ea8cc7b8c9c766785e3a9207a4aaba32ce48e
trejoaxel <axeltrejo@unomaha.edu>
2025-02-26 15:26:28 +0000

    completed DiceRoll

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,33 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name: Axel 
+#Date: 02/26/25
+#Assignment: DiceRoll
 import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  numRolls = 1000000
+
+  for count in range(numRolls):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    total = dice1 + dice2
+
+    rolls[total -2] = rolls[total -2] + 1
+
+
+    #print(dice1, dice2, total)
   #find the sum total of the two dice
   
-  #print statictics for dice rolls
 
+  #print statictics for dice rolls
+  num = 2
+  for r in rolls: 
+    precent = round(r / numRolls * 100, 1)
+    print(num , ":", r, precent)
+    num = num + 1
 
 if __name__ == '__main__':
   main()
